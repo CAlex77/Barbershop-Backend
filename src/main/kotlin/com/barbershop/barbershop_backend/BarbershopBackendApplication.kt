7ccc1317caa77a8fac98a37151dbb85691d2027f
CAlex77 <carlosalexandre1431.caf@gmail.com>
2025-10-16 01:27:32 -0400

    Inicializando o backend

---
+++ src/main/kotlin/com/barbershop/barbershop_backend/BarbershopBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.barbershop.barbershop_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BarbershopBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BarbershopBackendApplication>(*args)
+}
